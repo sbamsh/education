dec6d645fd1533db3985975f6b7173311ff1fba3
Saba Mirshahi <sabamirri@gmail.com>
2023-01-15 13:27:15 +0330

    nav bar codes got completed, header slick changed, responsive codes for nav and header added for 450px and 992px

--- main.js
+++ main.js
@@ -3,4 +3,19 @@ $(document).ready(function(){
    $('.navbar-toggler-btn').click(function(){
      $('.navbar-collapse').slideToggle(300);
    });
+   // change navbar bg
+   $(document).scroll(function(){
+    var $nav = $('.navbar');
+    $nav.toggleClass('change-on-scroll', $(this).scrollTop() > $nav.height());
+   });
+
+   // header slider
+   $('.header-banner-wrapper').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true 
+   });
+   
 });
